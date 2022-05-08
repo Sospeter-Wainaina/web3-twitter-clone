9c93d3e6c754285f10ca2c7ea7195ca06b9e8da8
sospeter-wainaina <sospetergathungu2000@gmail.com>
2022-05-08 04:15:06 -0400

    Twitter clone UI

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Sidebar from "../components/Sidebar"
+const style = {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`
+}
+
+
+
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <Sidebar />
+        <h1>Feed</h1>
+        <h1>Widgets</h1>
+      </div>
+  </div>
+
+  )
+  
+}
